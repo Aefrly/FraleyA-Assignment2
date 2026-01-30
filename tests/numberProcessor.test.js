47ff14a229cb13957084d80594261d5d2d5c0132
Aefrly <fraley.allison@ic.edu>
2026-01-30 08:24:01 -0600

    Create tests for totalSum, highestNumber, lowestNumber, and average functions

---
+++ tests/numberProcessor.test.js
@@ -0,0 +1,21 @@
+const { totalSum, highestNumber, lowestNumber, average } = require('./numberProcessor.js');
+
+test('should find total sum of numbers', () => {
+    const result = totalSum([1, 2, 3, 4, 5]);
+    expect(result).toBe(15);
+});
+
+test('should find highest number', () => {
+    const result = highestNumber([1, 2, 3, 4, 5]);
+    expect(result).toBe(5);
+});
+
+test('should find lowest number', () => {
+    const result = lowestNumber([1, 2, 3, 4, 5]);
+    expect(result).toBe(1);
+}); 
+
+test('should find average of numbers', () => {
+    const result = average([1, 2, 3, 4, 5]);
+    expect(result).toBe(3);
+});
